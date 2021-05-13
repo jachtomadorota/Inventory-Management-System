d4880fad7a88a0744741a63557c422fcf09816a4
jachtomadorota <jachtoma.do@gmail.com>
2021-05-13 23:06:24 +0200

    add fields in Product java class, add getters and setters

--- src/main/java/com/dorota/inventorymanagementsystem/model/Product.java
+++ src/main/java/com/dorota/inventorymanagementsystem/model/Product.java
@@ -5,9 +5,110 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
+import java.math.BigDecimal;
+
 @AllArgsConstructor
 @NoArgsConstructor
 
 public class Product {
+
+    private String id;
+
+    private String EAN;
+
+    private String name;
+
+    private String producer;
+
+    private String category;
+
+    private int VAT;
+
+    private BigDecimal purchasePriceNetto;
+
+    private BigDecimal purchasePriceBrutto;
+
+    private BigDecimal sellPriceRetail;
+
+    private BigDecimal sellPriceWholesale;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getEAN() {
+        return EAN;
+    }
+
+    public void setEAN(String EAN) {
+        this.EAN = EAN;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getProducer() {
+        return producer;
+    }
+
+    public void setProducer(String producer) {
+        this.producer = producer;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public int getVAT() {
+        return VAT;
+    }
+
+    public void setVAT(int VAT) {
+        this.VAT = VAT;
+    }
+
+    public BigDecimal getPurchasePriceNetto() {
+        return purchasePriceNetto;
+    }
+
+    public void setPurchasePriceNetto(BigDecimal purchasePriceNetto) {
+        this.purchasePriceNetto = purchasePriceNetto;
+    }
+
+    public BigDecimal getPurchasePriceBrutto() {
+        return purchasePriceBrutto;
+    }
+
+    public void setPurchasePriceBrutto(BigDecimal purchasePriceBrutto) {
+        this.purchasePriceBrutto = purchasePriceBrutto;
+    }
+
+    public BigDecimal getSellPriceRetail() {
+        return sellPriceRetail;
+    }
+
+    public void setSellPriceRetail(BigDecimal sellPriceRetail) {
+        this.sellPriceRetail = sellPriceRetail;
+    }
+
+    public BigDecimal getSellPriceWholesale() {
+        return sellPriceWholesale;
+    }
+
+    public void setSellPriceWholesale(BigDecimal sellPriceWholesale) {
+        this.sellPriceWholesale = sellPriceWholesale;
+    }
 }
