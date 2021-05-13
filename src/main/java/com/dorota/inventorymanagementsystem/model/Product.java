7a4a08310d0ad9faf2e7c539d9f4416030837c62
jachtomadorota <jachtoma.do@gmail.com>
2021-05-13 23:11:05 +0200

    add @Document annotation and @Id annotation for configuring model entity

--- src/main/java/com/dorota/inventorymanagementsystem/model/Product.java
+++ src/main/java/com/dorota/inventorymanagementsystem/model/Product.java
@@ -3,15 +3,19 @@ package com.dorota.inventorymanagementsystem.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Generated;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.math.BigDecimal;
 
 @AllArgsConstructor
 @NoArgsConstructor
-
+@Document(collection = "products")
 public class Product {
 
+    @Id
     private String id;
 
     private String EAN;
