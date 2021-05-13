113cce39d3cef77d1a098ec31823b14c6870d21f
jachtomadorota <jachtoma.do@gmail.com>
2021-05-13 23:15:47 +0200

    add ProductMongoRepostiory for managing mongo document

---
+++ src/main/java/com/dorota/inventorymanagementsystem/repository/ProductMongoRepository.java
@@ -0,0 +1,8 @@
+package com.dorota.inventorymanagementsystem.repository;
+
+import com.dorota.inventorymanagementsystem.model.Product;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface ProductMongoRepository extends MongoRepository<Product,String>{
+
+}
